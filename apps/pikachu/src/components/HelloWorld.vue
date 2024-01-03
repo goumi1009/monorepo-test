6faec26e2bd3bdf9519472169c1465278dec2d5f
goumi1009 <goumi1009@gmail.com>
2024-01-03 14:38:59 +0900

    hello world

--- apps/pikachu/src/components/HelloWorld.vue
+++ apps/pikachu/src/components/HelloWorld.vue
@@ -2,14 +2,15 @@
 defineProps({
   msg: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    <p>helloWorld dkdkdkdkdk</p>
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
